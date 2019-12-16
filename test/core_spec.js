60c3acc59884c8db64152588fdc7e5a60d0cdd39
Elco Garcia <garciaelco18@gmail.com>
2019-12-15 21:18:46 -0600

    next function to create a vote Map of state

--- test/core_spec.js
+++ test/core_spec.js
@@ -2,7 +2,7 @@ import { List, Map } from 'immutable';
 
 import { expect } from 'chai';
 
-import { setEntries } from '../src/core';
+import { setEntries, next } from '../src/core';
 
 describe('application logic', () => {
 	describe('setEntries', () => {
@@ -16,5 +16,22 @@ describe('application logic', () => {
 				})
 			);
 		});
+	});
+});
+
+describe('next', () => {
+	it('takes the next two entries under vote', () => {
+		const state = Map({
+			entries: List.of('Trainspotting', '28 Days of Later', 'Sunshine')
+		});
+		const nextState = next(state);
+		expect(nextState).to.equal(
+			Map({
+				vote: Map({
+					pair: List.of('Trainspotting', '28 Days Later')
+				}),
+				entries: List.of('Sunshine')
+			})
+		);
 	});
 });
