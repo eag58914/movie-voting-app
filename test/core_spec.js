3de20dcd2eefc08945da281298db30e405fa5852
Elco Garcia <garciaelco18@gmail.com>
2019-12-19 15:33:11 -0800

    test for votes to amke sure it increments right

--- test/core_spec.js
+++ test/core_spec.js
@@ -35,3 +35,50 @@ describe('next', () => {
 		);
 	});
 });
+
+describe('vote', () => {
+	const state = Map({
+		vote: Map({
+			pair: List.of('Trainspotting', '28 Days Later')
+		}),
+		entries: List()
+	});
+	const nextState = vote(state, 'Trainspotting', '28 Days Later');
+	expect(nextState).to.equal(
+		Map({
+			vote: Map({
+				pair: List.of('TrainSpotting', '28 Days Later'),
+				tally: Map({
+					Trainspotting: 1
+				})
+			}),
+			entries: List()
+		})
+	);
+});
+
+it('adds to existing tally for the voted entry', () => {
+	const state = Map({
+		vote: Map({
+			pair: List.of('Trainspotting', '28 Days Later'),
+			tally: Map({
+				Trainspotting: 3,
+				'28 Days Later': 2
+			})
+		}),
+		entries: List()
+	});
+	const nextState = vote(state, 'Trainspotting');
+	expect(nextState).to.equal(
+		Map({
+			vote: Map({
+				pair: List.of('Trainspotting', '28 Days Later'),
+				tally: Map({
+					Trainspotting: 4,
+					'28 Days Later': 2
+				})
+			}),
+			entries: List()
+		})
+	);
+});
