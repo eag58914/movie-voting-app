ec0c8a51e2fd61f225bdcd71e2e5d7e5498ea4dc
Elco Garcia <garciaelco18@gmail.com>
2019-12-19 15:06:28 -0800

    created next function for merging states

--- src/core.js
+++ src/core.js
@@ -1,4 +1,14 @@
 //sets entries key in the state Mapand set the value as the given List of entries
+import { List, Map } from 'immutable';
+
 export function setEntries(state, entries) {
-	return state.set('entries', entries);
+	return state.set('entries', List(entries));
+}
+//this function takes in thee old state and merges with the new set of entries
+export function next(state) {
+	const entries = state.get('entires');
+	return state.merge({
+		vote: Map({ pair: entries.take(2) }),
+		entries: entries.skip(2)
+	});
 }
