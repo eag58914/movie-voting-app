1dd8c743c03e22ee725c0d3b175115f911a097cb
Elco Garcia <garciaelco18@gmail.com>
2019-12-20 13:02:09 -0800

    function to update votes

--- src/core.js
+++ src/core.js
@@ -6,9 +6,13 @@ export function setEntries(state, entries) {
 }
 //this function takes in thee old state and merges with the new set of entries
 export function next(state) {
-	const entries = state.get('entires');
+	const entries = state.get('entries');
 	return state.merge({
 		vote: Map({ pair: entries.take(2) }),
 		entries: entries.skip(2)
 	});
+}
+
+export function vote(state, entry) {
+	return state.updateIn([ 'vote', 'tally', entry ], 0, (tally) => tally + 1);
 }
