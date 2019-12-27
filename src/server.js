55d0304207a25d669664cad0cf7bdaeabd5bd08e
Elco Garcia <garciaelco18@gmail.com>
2019-12-27 16:03:58 -0600

    store acquires state and actions

--- src/server.js
+++ src/server.js
@@ -1,5 +1,14 @@
 import Server from 'socket.io';
+import { store } from '..';
 
 export default function startServer() {
 	const io = new Server().attach(8090);
 }
+//connecting socket io to store to record actions
+store.subscribe(() => io.emit('state', store.getState().toJS()));
+
+// emit current state
+io.on('connection', (socket) => {
+	socket.emit('state', store.getState().toJS());
+	socket.on('action', store.dispatch.bind(store));
+});
